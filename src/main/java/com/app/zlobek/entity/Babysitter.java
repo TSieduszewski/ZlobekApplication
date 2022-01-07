6ea8806a3415991ed2107ee374c40d2d209648a3
TSieduszewski <62184872+TSieduszewski@users.noreply.github.com>
2022-01-07 12:02:00 +0100

    Dodanie encji Babysitter

--- src/main/java/com/app/zlobek/entity/Babysitter.java
+++ src/main/java/com/app/zlobek/entity/Babysitter.java
@@ -1,4 +1,27 @@
 package com.app.zlobek.entity;
 
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "babysitters")
 public class Babysitter {
+
+    @Id
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "start_shift")
+    private LocalDateTime startShift;
+
+    @Column(name = "end_shift")
+    private LocalDateTime endShift;
 }
