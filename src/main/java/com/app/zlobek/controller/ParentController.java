e4d1d59b48839980c53beb3a26a6ff61c7a656e9
TSieduszewski <62184872+TSieduszewski@users.noreply.github.com>
2022-01-27 20:53:12 +0100

    HOTFIX: dodanie stronicowania do wiadomości rodziców

--- src/main/java/com/app/zlobek/controller/ParentController.java
+++ src/main/java/com/app/zlobek/controller/ParentController.java
@@ -1,25 +1,34 @@
 package com.app.zlobek.controller;
 
+import com.app.zlobek.entity.Message;
 import com.app.zlobek.entity.Parent;
+import com.app.zlobek.service.MessageService;
 import com.app.zlobek.service.ParentService;
 import com.app.zlobek.util.messages.MessageWithReceivers;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 @RequestMapping("/parents")
 public class ParentController {
 
     private ParentService parentService;
+    private MessageService messageService;
 
     @Autowired
-    public ParentController(ParentService parentService) {
+    public ParentController(ParentService parentService, MessageService messageService) {
         this.parentService = parentService;
+        this.messageService = messageService;
     }
 
     @GetMapping("/list")
@@ -35,10 +44,27 @@ public class ParentController {
     }
 
     @GetMapping("/showSingleParentMessages")
-    public String singleParentMessages(@RequestParam("parentId") int theId, Model model){
+    public String singleParentMessages(@RequestParam("parentId") int theId,
+                                       @RequestParam("page") Optional<Integer> page,
+                                       @RequestParam("size") Optional<Integer> size,
+                                       Model model){
+        int currentPage = page.orElse(1);
+        int pageSize = size.orElse(5);
+
         Parent tempParent = parentService.findById(theId);
         model.addAttribute("singleParent", tempParent);
-        model.addAttribute("messages", tempParent.getMessages());
+
+        Page<Message> messagePage = messageService.findPaginated(PageRequest.of(currentPage-1,pageSize), tempParent.getMessages());
+
+        model.addAttribute("messages", messagePage);
+
+        int totalPages = messagePage.getTotalPages();
+        if (totalPages > 0) {
+            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
+                    .boxed()
+                    .collect(Collectors.toList());
+            model.addAttribute("pageNumbers", pageNumbers);
+        }
 
         return "parents/messages";
     }
